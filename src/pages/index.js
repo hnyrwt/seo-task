a179748620cd55b038aaaedded6d7e3abfedaebb
hnyrwt01 <hnyrwt01@gmail.com>
2024-09-24 14:47:28 +0530

    added button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
-
-
+import Link from 'next/link';
 
 export default function Home() {
-  return <>home</>;
+  return (
+    <div className='flex justify-center items-center flex-col gap-2 h-screen'>
+      <h1 className="text-2xl font-bold capitalize"> this is a Home page</h1>
+      <Link href="/products">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:scale-125">
+          Go to Products
+        </button>
+      </Link>
+    </div>
+  );
 }
